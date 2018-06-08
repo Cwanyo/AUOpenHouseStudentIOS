67d662ce57ab00bbf669c5d3ce5fc5f5576f55bb
ios-project <ios-project@Vilis-MacBook.local>
2018-06-09 00:50:12 +0700

    edit sidemenu

--- AUOpenHouseStudentIOS/Views/SideMenuTableViewController.swift
+++ AUOpenHouseStudentIOS/Views/SideMenuTableViewController.swift
@@ -6,8 +6,42 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
+import Kingfisher
 
 class SideMenuTableViewController: UITableViewController {
+    
+    
+    @IBOutlet weak var img_profile: UIImageView!
+    @IBOutlet weak var lb_name: UILabel!
+    
+    var handle: AuthStateDidChangeListenerHandle?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Rounded user image
+        img_profile.layer.cornerRadius = img_profile.frame.width / 2
+        img_profile.clipsToBounds = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        handle = Auth.auth().addStateDidChangeListener { (auth, user) in
+            if(user != nil){
+                
+                // set picture and name of the user
+                let resource = ImageResource(downloadURL: (user?.providerData[0].photoURL)!, cacheKey: user?.providerData[0].photoURL?.absoluteString)
+                self.img_profile.kf.setImage(with: resource)
+                
+                self.lb_name.text = user?.displayName
+            }
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        Auth.auth().removeStateDidChangeListener(handle!)
+    }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         NotificationCenter.default.post(name: NSNotification.Name("ToggleSideMenu"), object: nil)
